6af553b297e29b7ae7abb931423c671adabae64e
Lindelani <lindelani@gofuseit.com>
2023-03-20 15:22:32 +0200

    Pushed to remote

--- client/client_chat/lib/helpers/s3_image.dart
+++ client/client_chat/lib/helpers/s3_image.dart
@@ -1,21 +1,11 @@
 import 'dart:convert';
-import 'dart:typed_data';
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
-import 'package:aws_s3_api/s3-2006-03-01.dart';
 
 const bucketName = 'server-tracks';
 const avatarKey = 'amapiano/avatar/amapiano_avatar.jpeg';
 
 class S3Image {
   final String url;
-  final S3 _client = S3(
-    region: 'eu-west-1',
-    credentials: AwsClientCredentials(
-      accessKey: 'AKIAW6NI2VNC5JSPBTPQ',
-      secretKey: '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',
-    ),
-  );
 
   S3Image(this.url);
 
@@ -28,18 +18,6 @@ class S3Image {
     final response = await _getImage();
 
     final data = base64Encode(response.bodyBytes);
-    /*GetObjectOutput response = await _client
-        .getObject(
-      bucket: bucketName,
-      key: avatarKey,
-    )
-        .then((value) => value, onError: () {
-      print('Ran into an error');
-    });
-
-    Uint8List imageData = response.body!;
-
-    return base64Encode(imageData);*/
 
     return data;
   }
